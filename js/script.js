2a99f5a33b8507b865a6d8821b4ae8f5e08c35f5
isha0207 <isha95419@gmail.com>
2026-03-10 22:36:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log("Day 1 Task Loaded");
+document.addEventListener("DOMContentLoaded", function(){
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", function(){
+
+navMenu.classList.toggle("nav-active");
+menuToggle.classList.toggle("active");
+
+const expanded =
+menuToggle.getAttribute("aria-expanded") === "true" || false;
+
+menuToggle.setAttribute("aria-expanded", !expanded);
+
+});
+
+
+// Auto close menu when link clicked
+
+const navLinks = navMenu.querySelectorAll("a");
+
+navLinks.forEach(link => {
+
+link.addEventListener("click", () => {
+
+navMenu.classList.remove("nav-active");
+menuToggle.classList.remove("active");
+
+menuToggle.setAttribute("aria-expanded", "false");
+
+});
+
+});
+
+});
